26208652940c556b427bd2c9f5329440b9571282
sookyeong <sookyeong0505@gmail.com>
2025-01-15 17:12:37 +0900

    클래스 기반 뷰를 생성 - 간단한 get, post 메서드 구현

--- littlelemon/menuAPI/views.py
+++ littlelemon/menuAPI/views.py
@@ -1,12 +1,23 @@
+from tkinter import Menu
+
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def menu(request):
     return Response('This is the menu', status=status.HTTP_200_OK)
+
+
+class MenuList(APIView):
+    def get(self, request):
+        return Response({'menus': 'This is the menu'}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({'menus': 'New menu is created'}, status=status.HTTP_201_CREATED)
