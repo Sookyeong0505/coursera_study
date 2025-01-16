de91f02ace5f485ea331d396f8e2ba2bb5df5c77
sookyeong <sookyeong0505@gmail.com>
2025-01-16 11:28:23 +0900

    시리얼라이저를 사용해서 DB 레코드를 JSON 데이터로 변환해 반환하기

--- littlelemon/menuAPI/urls.py
+++ littlelemon/menuAPI/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('menu', views.menu_items),
+    path('menu/<int:id>', views.single_item),
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemsView.as_view()),
 ]
--- littlelemon/menuAPI/views.py
+++ littlelemon/menuAPI/views.py
@@ -1,9 +1,24 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.decorators import api_view
+
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
 
-# Create your views here.
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+
+@api_view()
+def single_item(request, id):
+    item = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
 
 
 # generics.ListCreateAPIView: 레코드를 표시하고 새 레코드를 만들기 위한 POST 호출 수락
